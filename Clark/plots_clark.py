Fix incorrect calculation of slope

Clark/plots_clark.py
@@ -6,6 +6,7 @@ import matplotlib.dates as mdates
 from mpld3 import plugins, utils
 from datetime import datetime, timedelta
 import custom_plugins as mplm
+from scipy.stats import linregress
 
 #define function to calculate a 5-day moving average
 def moving_average(a, n=5) :
@@ -51,9 +52,12 @@ for i in range(len(last_14_days)):
 		else:
 			trend.append(last_14_days[i] - last_14_days[i-1])
 
-if sum(trend) > 0:
+x_vals = [i for i in range(len(last_14_days))]
+slope, intercept, r_value, p_value, std_err = linregress(x_vals, last_14_days)
+
+if slope > 0:
 	trend_color = '#faafaf'
-elif sum(trend) <= 0:
+elif slope <= 0:
 	trend_color = '#affaaf'
 
 